b2b49cc151900064a1d0bd6389041e8ee3c959c2
TriaX <bagamag12@gmail.com>
2024-07-03 03:05:54 +0300

    update 2.5 refactor all code

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var originalProfileText = document.querySelector('.profile-text').innerHTML;
+document.addEventListener('DOMContentLoaded', () => {
+    const profileTextElement = document.querySelector('.profile-text');
+    const originalProfileText = profileTextElement.innerHTML;
+    const icons = document.querySelectorAll('.icon');
+    const plansButton = document.getElementById('plans-button');
+    const profileButton = document.getElementById('profile-button');
 
-    var icons = document.querySelectorAll('.icon');
-    var plansButton = document.getElementById('plans-button');
-    var profileButton = document.getElementById('profile-button');
-
-    // Add dimmed class to profile button by default
     profileButton.classList.add('dimmed');
+
+    const toggleActiveClass = (element) => {
+        icons.forEach(icon => icon.classList.remove('active', 'dimmed'));
+        element.classList.add('active');
+        if (element === profileButton) {
+            profileButton.classList.add('dimmed');
+        } else {
+            plansButton.classList.add('dimmed');
+        }
+    };
 
     icons.forEach(icon => {
         icon.addEventListener('click', function() {
-            icons.forEach(i => i.classList.remove('active'));
-            icons.forEach(i => i.classList.remove('dimmed'));
-            this.classList.add('active');
+            toggleActiveClass(this);
+            if (this === plansButton) {
+                updateProfileText("<b>My plans</b><br>1. Learn JavaScript.<br>2. Learn HTML and CSS.<br>3. Learn Bootstrap, React.<br>4. Learn SQL, Databases, Python, Java, PHP.<br>5. Learn Git, other frameworks and libraries in languages.");
+            } else {
+                updateProfileText(originalProfileText);
+            }
         });
     });
 
-    plansButton.addEventListener('click', function() {
-        var updateElement = document.querySelector('.profile-text');
-
-        updateElement.classList.add('hidden');
-        setTimeout(function() {
-            updateElement.innerHTML = "<b>My plans</b><br>1. Learn JavaScript.<br>2. Learn HTML and CSS.<br>3. Learn Bootstrap, React.<br>4. Learn SQL, Databases, Python, Java, PHP.<br>5. Learn Git, other frameworks and libraries in languages.";
-            updateElement.classList.remove('hidden');
+    const updateProfileText = (text) => {
+        profileTextElement.classList.add('hidden');
+        setTimeout(() => {
+            profileTextElement.innerHTML = text;
+            profileTextElement.classList.remove('hidden');
         }, 300);
-
-        // Update active/dimmed classes
-        icons.forEach(i => i.classList.remove('active'));
-        icons.forEach(i => i.classList.remove('dimmed'));
-        plansButton.classList.add('active');
-    });
-
-    profileButton.addEventListener('click', function() {
-        var updateElement = document.querySelector('.profile-text');
-
-        updateElement.classList.add('hidden');
-        setTimeout(function() {
-            updateElement.innerHTML = originalProfileText;
-            updateElement.classList.remove('hidden');
-        }, 300);
-
-        // Update active/dimmed classes
-        icons.forEach(i => i.classList.remove('active'));
-        icons.forEach(i => i.classList.remove('dimmed'));
-        profileButton.classList.add('active');
-        profileButton.classList.add('dimmed');
-    });
+    };
 });
